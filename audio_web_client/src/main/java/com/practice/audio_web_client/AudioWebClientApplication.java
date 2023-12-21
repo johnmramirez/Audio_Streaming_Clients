4163db478e9d4c63b6adef61ffa935d4f7480f46
johnmramirez <johnmichaelramirez7@gmail.com>
2023-12-20 18:33:52 -0600

    Initial Commit

---
+++ audio_web_client/src/main/java/com/practice/audio_web_client/AudioWebClientApplication.java
@@ -0,0 +1,13 @@
+package com.practice.audio_web_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudioWebClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudioWebClientApplication.class, args);
+	}
+
+}
